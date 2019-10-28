02cb7839860fa134c07df42346365984d626ecd9
Marius <mariaus.vai@gmail.com>
2019-10-28 12:01:30 +0200

    Add profile

--- client-app/src/features/profiles/ProfileDescription.tsx
+++ client-app/src/features/profiles/ProfileDescription.tsx
@@ -1,12 +1,12 @@
 import React, { useContext, useState } from 'react';
 import { Tab, Grid, Header, Button } from 'semantic-ui-react';
 import { RootStoreContext } from '../../app/stores/rootStore';
-//import ProfileEditForm from './ProfileEditForm';
+import ProfileEditForm from './ProfileEditForm';
 import { observer } from 'mobx-react-lite';
 
 const ProfileDescription = () => {
   const rootStore = useContext(RootStoreContext);
-  const {  profile, isCurrentUser } = rootStore.profileStore;
+  const {updateProfile, profile, isCurrentUser } = rootStore.profileStore;
   const [editMode, setEditMode] = useState(false);
   return (
     <Tab.Pane>
@@ -26,13 +26,13 @@ const ProfileDescription = () => {
             />
           )}
         </Grid.Column>
-        {/* <Grid.Column width={16}>
+        <Grid.Column width={16}>
           {editMode ? (
             <ProfileEditForm updateProfile={updateProfile} profile={profile!} />
           ) : (
             <span>{profile!.bio}</span>
           )}
-        </Grid.Column> */}
+        </Grid.Column>
       </Grid>
     </Tab.Pane>
   );
